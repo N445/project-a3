220ad86fc73538d1f6f6556c51554699fc143622
Robin PARISOT <rparisot@nvl.fr>
2024-04-05 13:19:21 +0000

    WIP integrate mercure

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -4,13 +4,22 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mercure\HubInterface;
+use Symfony\Component\Mercure\Update;
 use Symfony\Component\Routing\Attribute\Route;
 
 class HomepageController extends AbstractController
 {
     #[Route('/', name: 'APP_HOMEPAGE')]
-    public function index(): Response
+    public function index(HubInterface $hub): Response
     {
+        $update = new Update(
+            'https://project-a3.local/books/1',
+            json_encode(['status' => 'OutOfStock'])
+        );
+
+        $hub->publish($update);
+
         return $this->render('homepage/index.html.twig', [
             'controller_name' => 'HomepageController',
         ]);
